a399d29edfc36a5bebeeebdbda3c5d682ccfcc6a
Samuel Resende <sresende@knights.ucf.edu>
2017-08-01 12:42:40 -0400

    added new user authentication packages

--- server/server.js
+++ server/server.js
@@ -6,9 +6,15 @@ var morgan = require('morgan');             // log requests to the console (expr
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var cors = require('cors');
+var moment = require('moment');
+
+var databaseConfig = require('./config/database');
+var router = require('./app/routes')
+
  
 // Configuration
-mongoose.connect('mongodb://localhost/userProgress');
+//mongoose.connect('mongodb://localhost/userProgress');
+mongoose.connect(databaseConfig.url);
  
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
@@ -16,6 +22,8 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 app.use(cors());
+
+router(app);
  
 app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
